542c383348162d12f4b3d89cd762a9df913b6d72
MonkeyDLuffy1230 <131762587+MonkeyDLuffy1230@users.noreply.github.com>
2023-05-09 15:41:27 +0530

    Update random.java

--- random.java
+++ random.java
@@ -3,6 +3,10 @@ public class random {
     int a=70;
     int b=80;
     int ans=a+b;
-    System.out.println("sum is"+ans);
+    float ans1=a/b;
+    int ans2=a-b;
+    System.out.println("sum is "+ans);
+    System.out.println("quotient is "+ans1);
+    System.out.println("difference is "+ans2);
   }
 }
